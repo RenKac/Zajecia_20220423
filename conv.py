6c2b55fd6c971ba03d1458da676ac1d4d3ed1a65
vdi-student <vdi-student@EDU-WSB.local>
2022-04-23 15:33:10 +0200

    kom3

--- conv.py
+++ conv.py
@@ -1,5 +1,5 @@
 def zloty_to_dolar(DOLAR):
     kurs = 4.56
     PLN = 300
-    DOLARY = kurs*PLN
+    DOLAR = kurs*PLN
     return DOLAR
--- programy.py
+++ programy.py
@@ -1,3 +1,10 @@
 from conv import *
 from przelicz import *
 
+x = float(input('Ile masz zlotowek'))
+print(x)
+
+dolar = zloty_to_dolar(x)
+print(dolar)
+
+
